755d21810ddea8989ce81c11e79e624a5be40e76
Christoph Finkensiep <chfin@freenet.de>
2014-02-08 21:48:34 +0100

    xmodmap and info.org

--- xmonad.hs
+++ xmonad.hs
@@ -4,20 +4,29 @@ import System.Posix.Env (getEnv)
 import Data.Maybe (maybe)
 
 import XMonad
-import XMonad.Config.Desktop
-import XMonad.Config.Gnome
-import XMonad.Config.Kde
+--import XMonad.Config.Desktop
+--import XMonad.Config.Gnome
+--import XMonad.Config.Kde
 import XMonad.Config.Xfce
 
 main = do
-     session <- getEnv "DESKTOP_SESSION"
-     xmonad  $ maybe desktopConfig desktop session
+     xmonad $ xfceConfig
+     	    { terminal = "urxvt"
+    	    , modMask  = mod3Mask
+	    , startupHook = startup
+    	    }
 
-desktop "gnome" = gnomeConfig
-desktop "kde" = kde4Config
-desktop "xfce" = xfceConfig
-    { terminal = "urxvt"
-,     modMask  = mod4Mask
-    }
-desktop "xmonad-mate" = gnomeConfig
-desktop _ = desktopConfig
+startup :: X()
+startup = do
+	spawn "xmodmap -e \"remove Mod4 = Hyper_L\" -e \"add Mod3 = Hyper_L\""
+
+--main = do
+--     session <- getEnv "DESKTOP_SESSION"
+--     xmonad  $ maybe desktopConfig desktop session
+
+--desktop "gnome" = gnomeConfig
+--desktop "kde" = kde4Config
+--desktop "xfce" = xfceConfig
+
+--desktop "xmonad-mate" = gnomeConfig
+--desktop _ = desktopConfig
