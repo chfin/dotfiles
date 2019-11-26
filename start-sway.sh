1d719e603654e60796680fa238075ebe2ecba07e
Christoph Finkensiep <chfin@chfin.de>
2019-11-26 16:59:15 +0100

    small updates to sway config

--- start-sway.sh
+++ start-sway.sh
@@ -13,6 +13,8 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 export SSH_AUTH_SOCK
 
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+
 PATH=$HOME/.local/bin:$PATH
 export PATH
 
