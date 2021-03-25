1ab31a058356670e13aae50efbd22a62d930e8bc
Christoph Finkensiep <chfin@chfin.de>
2021-03-25 20:48:48 +0100

    make some improvements to sway config

--- start-sway.sh
+++ start-sway.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 export XDG_CURRENT_DESKTOP=sway
 export QT_STYLE_OVERRIDE=gtk
 export QT_QPA_PLATFORM=wayland-egl
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_FONT_DPI=120
 #export QT_SCALE_FACTOR=1.5
 export CLUTTER_BACKEND=wayland
 #export GDK_DPI_SCALE=1.5
@@ -11,14 +12,16 @@ export MOZ_USE_XINPUT2=1
 export MOZ_ENABLE_WAYLAND=1
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
+# eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+# export SSH_AUTH_SOCK
 
-. $HOME/.nix-profile/etc/profile.d/nix.sh
+. /etc/profile
 
-export JULIA_PKG_SERVER=pkg.julialang.org
 
-PATH=$HOME/.local/bin:$PATH
-export PATH
+# export JULIA_PKG_SERVER=pkg.julialang.org
+
+# PATH=$HOME/.local/bin:$PATH
+# PATH=$HOME/.ghcup/bin:$PATH
+# export PATH
 
 sway > /var/log/sway.log 2>&1
